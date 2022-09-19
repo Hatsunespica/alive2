1b9e72b598f95951a0c5f7a96d2ce59ced1849a8
spica <Spica>
2022-09-19 13:50:52 -0700

    upload linkage from common to external

--- tools/alive-mutate.cpp
+++ tools/alive-mutate.cpp
@@ -484,7 +484,7 @@ bool inputVerify() {
             valid = true;
             if (fit->getLinkage() ==
                 llvm::GlobalValue::LinkageTypes::InternalLinkage) {
-              fit->setLinkage(llvm::GlobalValue::CommonLinkage);
+              fit->setLinkage(llvm::GlobalValue::ExternalLinkage);
             }
           }
         }
